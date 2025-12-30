509ef64a8022005bfb834159edbc1656a11677f1
LargeSsun <wkjang74@naver.com>
2025-12-30 00:46:58 +0000

    Update Dockerfile, nginx configuration, and Kubernetes manifests to enhance service setup and management

---
+++ delete.sh
@@ -0,0 +1,7 @@
+kubectl delete -f k8s/db.yaml
+kubectl delete -f k8s/pv.yaml
+kubectl delete -f k8s/auth.yaml
+kubectl delete -f k8s/employee.yaml
+kubectl delete -f k8s/photo.yaml
+kubectl delete -f k8s/gateway.yaml
+kubectl delete -f k8s/nginx.yaml
---
+++ run.sh
@@ -0,0 +1,7 @@
+kubectl apply -f k8s/pv.yaml
+kubectl apply -f k8s/db.yaml
+kubectl apply -f k8s/auth.yaml
+kubectl apply -f k8s/employee.yaml
+kubectl apply -f k8s/photo.yaml
+kubectl apply -f k8s/gateway.yaml
+kubectl apply -f k8s/nginx.yaml
